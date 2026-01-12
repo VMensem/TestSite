5c965bc00298d14341985547e4d7003f68f33a48
Vladyslav <124316302+VMensem@users.noreply.github.com>
2026-01-12 18:51:53 +0200

    Update app.py

--- app.py
+++ app.py
@@ -121,4 +121,6 @@ def page_not_found(e):
 
 # --------------------------- 🚀 Запуск ---------------------------
 if __name__ == "__main__":
-    app.run(host="127.0.0.1", port=5000, debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
